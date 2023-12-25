1f3e5977296906de5ef0b4c827c9ecf426f755d8
eric.luo <eric.luo@nothing.tech>
2023-12-25 18:27:05 +0800

    merge techpack initial code
    Change-Id: Id29b1238e7b8cc4400f7088693cfad27439cc944
    (cherry picked from commit a6df3cb63e009f23a56d1d3dbee87aa490f684ea)
    (cherry picked from commit cea53c2dcabffbcf6398c376492425c222d50ea9)
    (cherry picked from commit ba7e44f3c02068d00520865d1bc6d667537e45a1)
    (cherry picked from commit 279b7958a0e2731718c9b45a129ab3d400bad21f)
    (cherry picked from commit 24aeab8dafa69cf79637a03d399cf2c1cec91b25)
    (cherry picked from commit 429c4b42d801e7770fe904b2f06eaafdc6c976da)

---
+++ techpack/dataipa/config/dataipa.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#define CONFIG_GSI 1
+#define CONFIG_GSI_REGISTER_VERSION_2 1
+#define CONFIG_RMNET_IPA3 1
+#define CONFIG_RNDIS_IPA 1
+#define CONFIG_IPA_WDI_UNIFIED_API 1
+#define CONFIG_IPA3_REGDUMP 1
+#define CONFIG_IPA3_APPS_REGDUMP 1
+#define CONFIG_IPA3_REGDUMP_IPA_4_5 1
+#define CONFIG_IPA3_4_5_RGSTR 0
+#define CONFIG_IPA3_REGDUMP_NUM_EXTRA_ENDP_REGS 0
---
+++ techpack/display/msm/sde/sde_encoder_dce.h
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2016-2017, 2020 The Linux Foundation. All rights reserved.
+ */
+
+#ifndef __SDE_ENCODER_DCE_H__
+#define __SDE_ENCODER_DCE_H__
+
+#include "sde_encoder.h"
+
+/**
+ * sde_encoder_dce_set_bpp : set src_bpp and target_bpp in sde_crtc
+ * @msm_mode_info: Mode info
+ * @crtc: Pointer to drm crtc structure
+ */
+void sde_encoder_dce_set_bpp(
+		struct msm_mode_info mode_info, struct drm_crtc *crtc);
+
+/**
+ * sde_encoder_dce_disable : function to disable compression
+ * @sde_enc: pointer to virtual encoder structure
+ */
+void sde_encoder_dce_disable(struct sde_encoder_virt *sde_enc);
+
+/**
+ * sde_encoder_dce_setup : function to configure compression block
+ * @sde_enc: pointer to virtual encoder structure
+ * @params: pointer to kickoff params
+ */
+int sde_encoder_dce_setup(struct sde_encoder_virt *sde_enc,
+		struct sde_encoder_kickoff_params *params);
+
+/**
+ * sde_encoder_dce_flush :function to flush the compression configuration
+ * @sde_enc: pointer to virtual encoder structure
+ */
+void sde_encoder_dce_flush(struct sde_encoder_virt *sde_enc);
+
+/**
+ * sde_encoder_has_dsc_hw_rev_2 :checks if dsc_hw_rev_1_2 feature is enabled
+ * @sde_enc: pointer to virtual encoder structure
+ */
+bool sde_encoder_has_dsc_hw_rev_2(struct sde_encoder_virt *sde_enc);
+
+#endif /* __SDE_ENCODER_DCE_H__ */
