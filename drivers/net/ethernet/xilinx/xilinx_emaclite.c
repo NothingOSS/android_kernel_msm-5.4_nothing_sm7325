e5ca814e57b4e65b114d33ec96b5296af001f3fd
Mifacopy <mifacopy@users.noreply.github.com>
2026-03-08 23:52:04 +0800

    net: xilinx_emaclite: Do not print real IOMEM pointer
    Backported from https://github.com/torvalds/linux/commit/d0d62baa7f505bd4c59cd169692ff07ec49dde37

--- drivers/net/ethernet/xilinx/xilinx_emaclite.c
+++ drivers/net/ethernet/xilinx/xilinx_emaclite.c
@@ -1191,6 +1191,8 @@ static int xemaclite_of_probe(struct platform_device *ofdev)
 	}
 
 	dev_info(dev,
+		 "Xilinx EmacLite at 0x%08lX mapped to 0x%p, irq=%d\n",
+		 (unsigned long __force)ndev->mem_start, lp->base_addr, ndev->irq);
 		 "Xilinx EmacLite at 0x%08X mapped to 0x%p, irq=%d\n",
 		 (unsigned int __force)ndev->mem_start, lp->base_addr, ndev->irq);
 	return 0;
